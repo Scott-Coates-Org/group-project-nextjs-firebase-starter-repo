4c123ba2ec2a3d4b733a40dd299bcba267b7a311
Patrick R Bergstroem <45342505+prb01@users.noreply.github.com>
2022-10-21 09:38:49 -0300

    Initial commit

---
+++ components/Logout.jsx
@@ -0,0 +1,25 @@
+import { useDispatch } from 'react-redux';
+import { useRouter } from 'next/router';
+import { signOut } from 'firebase/auth';
+import { auth } from '../firebase/client';
+import { useAuth } from '../context/Auth';
+import { logout } from '../redux/authSlice';
+
+const Logout = ({ children }) => {
+  const dispatch = useDispatch();
+  const { clear } = useAuth();
+
+  const logoutHandler = () => {
+    signOut(auth);
+    clear();
+    dispatch(logout());
+  };
+
+  return (
+    <button onClick={() => logoutHandler()} className="button">
+      Logout
+    </button>
+  );
+};
+
+export default Logout;
